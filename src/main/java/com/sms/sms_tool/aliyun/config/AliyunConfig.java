7af5b03a680740524bd28d0148f021c1fcd00990
iak3ec <54735907+nu0l@users.noreply.github.com>
2024-11-25 15:57:08 +0800

    Update AliyunConfig.java

--- src/main/java/com/sms/sms_tool/aliyun/config/AliyunConfig.java
+++ src/main/java/com/sms/sms_tool/aliyun/config/AliyunConfig.java
@@ -1,8 +1,6 @@
 package com.sms.sms_tool.aliyun.config;
 
-/**
- * @author qiukai
- */
+
 public class AliyunConfig {
     public static final String ACCESS_KEY = "x";
     public static final String ACCESS_SECRET = "X";
